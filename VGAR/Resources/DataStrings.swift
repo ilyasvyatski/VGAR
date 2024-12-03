c2e4cd346d25cc525eb476fdad093966d735c06d
Ilya Svyatski <promonolit1234567@gmail.com>
2024-12-03 17:14:46 +0300

    Text descriptions changed

--- VGAR/Resources/DataStrings.swift
+++ VGAR/Resources/DataStrings.swift
@@ -8,5 +8,5 @@
 public struct DataStrings {
     public static let videosLink = "https://neoviso.com/arvideo/vg/"
     public static let examplePhotosLink = "https://neoviso.com/vg/"
-    public static let supportServiceLink = "https://gomelmedia.by/?page=pr&type=contact"
+    public static let supportServiceLink = "http://www.bybook.by/?page=about"
 }
